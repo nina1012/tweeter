44a0a7de6c15a913684d881ced71f319a6b3080c
Nina <labferment10@gmail.com>
2024-05-20 15:10:28 +0200

    rearranged folders and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { AppProvider } from './providers/app';
+import { AppProvider } from './app/main-provider';
+import { Button } from './components/ui/button';
 
 function App() {
-  return <AppProvider>Hello World</AppProvider>;
+  return (
+    <AppProvider>
+      <h1 className="text-red-400">Hello World</h1>
+      <Button>Click me</Button>
+    </AppProvider>
+  );
 }
 
 export default App;
