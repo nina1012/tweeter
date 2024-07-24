f3dad9b1437f8f1beebe7b52cc351b5816697645
Nina <labferment10@gmail.com>
2024-07-24 22:59:01 +0200

    added Reply type, update

--- src/features/tweet/types/index.ts
+++ src/features/tweet/types/index.ts
@@ -14,3 +14,14 @@ export type Tweet = {
   original_tweet_id?: string;
   original_author_id?: string;
 };
+
+export type Reply = {
+  content: string;
+  image?: FileList;
+  likes: string[];
+  is_reply: boolean;
+  created_at: Date;
+  author_id: string;
+  userID?: string;
+  originalTweetID?: string;
+};
