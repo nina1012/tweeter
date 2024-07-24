c072877ddec181a37f4754f9a389b9dba6e64f69
Nina <labferment10@gmail.com>
2024-07-24 23:13:01 +0200

    rearranged Like,Bookmark and Retweet buttons into this folder

---
+++ src/features/tweet/components/action-buttons/components/TweetLikeButton.tsx
@@ -0,0 +1,48 @@
+import clsx from 'clsx';
+import { Heart } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { useUser } from '@/features/auth/api/get-current-user';
+import {
+  useLikeTweet,
+  useUnlikeTweet,
+} from '@/features/tweet/api/like-unlike-tweet';
+import { Tweet } from '@/features/tweet/types';
+export type TweetLikeButtonProps = {
+  tweet: Tweet;
+};
+
+export const TweetLikeButton = ({ tweet }: TweetLikeButtonProps) => {
+  const { user } = useUser();
+
+  const isLiked = user?.id ? tweet.likes.includes(user?.id) : false;
+
+  const { likeTweet } = useLikeTweet();
+  const { unlikeTweet } = useUnlikeTweet();
+
+  const handleClick = () => {
+    if (!tweet || !user) return;
+    if (isLiked) {
+      unlikeTweet({
+        tweetID: tweet.tweet_id as string,
+        userID: user?.id as string,
+      });
+    } else {
+      likeTweet({
+        tweetID: tweet.tweet_id as string,
+        userID: user?.id as string,
+      });
+    }
+  };
+
+  return (
+    <Button
+      variant="ghost"
+      className={clsx(isLiked && 'text-red-500', 'tweet-button')}
+      onClick={handleClick}
+    >
+      <Heart />
+      <span className="tweet-button-icon">{isLiked ? 'Liked' : 'Like'}</span>
+    </Button>
+  );
+};
---
+++ src/features/tweet/components/action-buttons/components/TweetOptions.tsx
@@ -0,0 +1,53 @@
+import { Edit, Ellipsis, Trash } from 'lucide-react';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useNotifications } from '@/components/ui/notifications';
+
+import { useDeleteTweet } from '../../../api/delete-tweet';
+import { Tweet } from '../../../types';
+
+export type TweetOptionsProps = {
+  tweet: Tweet;
+};
+export const TweetOptions = ({ tweet }: TweetOptionsProps) => {
+  const { removeTweet } = useDeleteTweet();
+  const { addNotification } = useNotifications();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Ellipsis />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="z-[1000] flex flex-col !font-normal !text-gray-700 opacity-100">
+        <DropdownMenuItem
+          onClick={() => {
+            if (!tweet.tweet_id) return;
+            removeTweet({
+              tweetID: tweet.tweet_id,
+              originalAuthorID: tweet.original_author_id,
+            });
+            addNotification({
+              title: 'Deleted tweet',
+              type: 'success',
+              message: 'You successfully deleted a tweet',
+            });
+          }}
+          className="text-red-500"
+        >
+          <Trash />
+          <DropdownMenuLabel>Delete</DropdownMenuLabel>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Edit />
+          <DropdownMenuLabel>Edit</DropdownMenuLabel>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
