ba5704714c70aa68a0225f1e3d5458b6764b63c8
Nina <labferment10@gmail.com>
2024-08-05 00:02:23 +0200

    update

--- src/testing/mocks/handlers/auth.ts
+++ src/testing/mocks/handlers/auth.ts
@@ -2,7 +2,7 @@ import { StringQuery } from '@mswjs/data/lib/query/queryTypes';
 import { http, HttpResponse } from 'msw';
 
 import { db, persistDB } from '../db';
-import { authenticate, hash, networkDelay } from '../utils';
+import { hash, networkDelay } from '../utils';
 
 type LoginCredentials = {
   email: string;
@@ -19,50 +19,60 @@ type RegisterCredentials = {
 
 export const authHandlers = [
   // registration
-  http.post(`/auth/register`, async ({ request }) => {
-    await networkDelay();
-    try {
-      const user = (await request.json()) as RegisterCredentials;
+  http.post(
+    `${import.meta.env.VITE_SUPABASE_URL}/auth/v1/signup`,
+    async ({ request }) => {
+      await networkDelay();
+      try {
+        const user = (await request.json()) as RegisterCredentials;
+        console.log(user);
 
-      const userInDB = db.user.findFirst({
-        where: {
-          email: user.email as Partial<StringQuery>,
-        },
-      });
+        const userInDB = db.user.findFirst({
+          where: {
+            email: user.email as Partial<StringQuery>,
+          },
+        });
 
-      if (userInDB) {
+        if (userInDB) {
+          return HttpResponse.json(
+            { message: 'The user already exists' },
+            { status: 400 },
+          );
+        }
+
+        db.user.create({
+          ...user,
+          password: hash(user.password),
+        });
+
+        await persistDB('user');
+
         return HttpResponse.json(
-          { message: 'The user already exists' },
-          { status: 400 },
+          {
+            data: {
+              user: { email: user.email, password: user.password },
+              session: {
+                access_token: 'mock_access_token',
+                refresh_token: 'mock_refresh_token',
+              },
+            },
+          },
+          { status: 201 },
+        );
+      } catch (error: any) {
+        return HttpResponse.json(
+          { message: error?.message || 'Server error' },
+          { status: 500 },
         );
       }
-
-      db.user.create({
-        ...user,
-        password: hash(user.password),
-      });
-
-      await persistDB('user');
-
-      const result = authenticate({
-        email: user.email,
-        password: user.password,
-      });
-
-      return HttpResponse.json(result, { status: 201 });
-    } catch (error: any) {
-      return HttpResponse.json(
-        { message: error?.message || 'Server error' },
-        { status: 500 },
-      );
-    }
-  }),
+    },
+  ),
 
   // login
   http.post(
     `${import.meta.env.VITE_SUPABASE_URL}/auth/v1/token`,
     async ({ request }) => {
-      // await networkDelay();
+      await networkDelay();
       const { email, password } = (await request.json()) as LoginCredentials;
       if (!email || !password)
         return HttpResponse.json(
@@ -71,7 +81,15 @@ export const authHandlers = [
         );
 
       return HttpResponse.json(
-        { data: { user: { email, password }, session: null } },
+        {
+          data: {
+            user: { email, password },
+            session: {
+              access_token: 'mock_access_token',
+              refresh_token: 'mock_refresh_token',
+            },
+          },
+        },
         {
           status: 201,
         },
